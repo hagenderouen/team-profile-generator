357731ed4aa314aaf37a0a3d1649da98e60d9e81
Hagen DeRouen <hagend87@gmail.com>
2021-03-30 09:07:10 -0500

    Add starting assets.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should create an object with "name" string, "id" number, and "email" string', () => {
+            // Arrange
+
+            // Act
+
+            // Assert
+        })
+    })
+})
